6d64e39b2372bba28d22d1dc47c0efa4cabdcf8d
sabetts <sabetts>
2006-04-29 19:02:18 +0000

    (:stumpwm): export #:set-border-color

--- package.lisp
+++ package.lisp
@@ -1,6 +1,6 @@
 (defpackage :stumpwm
   (:use :cl)
   (:export #:stumpwm #:define-key #:undefine-key #:*root-map*
-	   #:*top-map* #:set-fg-color #:set-bg-color #:set-font
+	   #:*top-map* #:set-fg-color #:set-bg-color #:set-border-color #:set-font
 	   #:sync-keys #:*input-map* #:define-stumpwm-command #:kbd #:set-prefix-key))
 
