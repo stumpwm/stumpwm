82d4a3d503f8786b3decc16295531b8a0af01abf
Magnus Henoch <mange@freemail.hu>
2007-08-31 22:42:52 +0200

    MAKE-HEADS: compatibility with NetBSD sed

--- core.lisp
+++ core.lisp
@@ -2195,22 +2195,23 @@ FOCUS-WINDOW is an extra window used for _NET_SUPPORTING_WM_CHECK."
   (if (screen-heads screen)
     (mapcar #'copy-head (screen-heads screen))
     (cond
-      (and 
-       (xlib:query-extension *display* "XINERAMA")
-       (let* ((*package* (find-package :stumpwm))
-	      (heads (read-from-string
-		      (let ((*screen-list* (list screen)))
-			(run-prog-collect-output "/bin/sh" "-c" "echo -n '\('; xdpyinfo -ext XINERAMA | sed -n 's/^\\s\\+head #\\([[:digit:]]\\):\\s\\+\\([[:digit:]]\\+\\)x\\([[:digit:]]\\+\\)\\s*@\\s*\\([[:digit:]]\\+\\),\\([[:digit:]]\\+\\).*$/#S(head :number \\1 :width \\2 :height \\3 :x \\4 :y \\5)/p'; echo -n '\)'")))))
-	 ;; Ignore 'clone' heads.
-	 (setf heads (delete-duplicates heads
-					:test (lambda (h1 h2) (and (= (frame-height h1) (frame-height h2))
-							      (= (frame-width h1) (frame-width h2))
-							      (= (frame-x h1) (frame-x h2))
-							      (= (frame-y h1) (frame-y h2))))))
-	 heads))
+      ((and
+	 (xlib:query-extension *display* "XINERAMA")
+	 (let* ((*package* (find-package :stumpwm))
+		(heads (read-from-string
+			 (let ((*screen-list* (list screen)))
+			   ;; For compatibility with NetBSD sed, don't use the + operator.
+			   (run-prog-collect-output "/bin/sh" "-c" "echo -n '\('; xdpyinfo -ext XINERAMA | sed -n 's/^[[:space:]][[:space:]]*head #\\([[:digit:]]\\):[[:space:]][[:space:]]*\\([[:digit:]][[:digit:]]*\\)x\\([[:digit:]][[:digit:]]*\\)[[:space:]]*@[[:space:]]*\\([[:digit:]][[:digit:]]*\\),\\([[:digit:]][[:digit:]]*\\).*$/#S(head :number \\1 :width \\2 :height \\3 :x \\4 :y \\5)/p'; echo -n '\)'")))))
+	   ;; Ignore 'clone' heads.
+	   (setf heads (delete-duplicates heads
+					  :test (lambda (h1 h2) (and (= (frame-height h1) (frame-height h2))
+								     (= (frame-width h1) (frame-width h2))
+								     (= (frame-x h1) (frame-x h2))
+								     (= (frame-y h1) (frame-y h2))))))
+	   heads)))
       (t
-       ;; Xinerama not supported, or we got no information about
-       ;; heads for some other reason.
+	;; Xinerama not supported, or we got no information about
+	;; heads for some other reason.
 	(list (make-head :number 0
 			 :x (screen-x screen)
 			 :y (screen-y screen)
