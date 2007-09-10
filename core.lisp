947978af8b538e2d84042afb6e7e91fb93331e3e
Jonathan Moore Liles <wantingwaiting@users.sf.net>
2007-09-10 14:15:13 -0500

    Allow escaping of the color escape character.

--- core.lisp
+++ core.lisp
@@ -2115,6 +2115,7 @@ windows used to draw the numbers in. The caller must destroy them."
 	 (width 0)
 	 (gcontext (screen-message-gc screen))
 	 (message-win (screen-message-window screen)))
+    (set-color screen gcontext "n" 0)
     (loop for s in strings
 	  ;; We need this so we can track the row for each element
 	  for i from 0 to (length strings)
@@ -2122,16 +2123,17 @@ windows used to draw the numbers in. The caller must destroy them."
 	       (loop
 		 for st = 0 then (+ en (1+ off))
 		 as en = (position #\^ s :start st)
-		 do (progn 
-		      (when draw
-			(xlib:draw-image-glyphs message-win gcontext
-						(+ *message-window-padding* x)
-						(+ (* i height)
-						   (xlib:font-ascent (screen-font screen)))
-						(subseq s st en)
-						:translate #'translate-id
-						:size 16))
-		      (setf x (+ x (xlib:text-width (screen-font screen) (subseq s st en) :translate #'translate-id)))
+		 do (progn
+		      (let ((en (if (and en (eq #\^ (elt s (1+ en)))) (1+ en) en)))
+			(when draw
+			  (xlib:draw-image-glyphs message-win gcontext
+						  (+ *message-window-padding* x)
+						  (+ (* i height)
+						     (xlib:font-ascent (screen-font screen)))
+						  (subseq s st en)
+						  :translate #'translate-id
+						  :size 16))
+			(setf x (+ x (xlib:text-width (screen-font screen) (subseq s st en) :translate #'translate-id))))
 		      (when en
 			(setf off (set-color screen gcontext s (1+ en))))
 		      (setf width (max width x)))
@@ -2234,7 +2236,7 @@ FOCUS-WINDOW is an extra window used for _NET_SUPPORTING_WM_CHECK."
 ;; I won't explain here the many reasons that C is better than ANSI, so just
 ;; take my word for it.
 
-;; You can redefine these to whatever you like.
+;; Eight colors. You can redefine these to whatever you like.
 (defvar *colors*
   '("black"
     "red"
@@ -2260,41 +2262,52 @@ FOCUS-WINDOW is an extra window used for _NET_SUPPORTING_WM_CHECK."
     (setf (color-map-current (screen-colors screen)) (color-map-norm (screen-colors screen)))))
 
 (defun get-color (screen color)
-  (elt (color-map-current (screen-colors screen)) color))
+    (elt (color-map-current (screen-colors screen)) color))
 
 (defun get-bg-color (screen color)
   (setf (color-bg (screen-colors screen)) color)
-  (get-color screen color))
+  (if color
+    (get-color screen color)
+    (get-bg-color-pixel screen)))
 
 (defun get-fg-color (screen color)
   (setf (color-fg (screen-colors screen)) color)
-  (get-color screen color))
+  (if color
+    (get-color screen color)
+    (get-fg-color-pixel screen)))
 
 (defun set-color (screen gc s i)
-  (let ((l 2)
-	(f (subseq s i (1+ i)))
-	(b (subseq s (1+ i) (+ i 2))))
-    (case (elt f 0)
-      (#\n ; normal
-       (setf f "*" b "*" l 1
-	     (color-map-current (screen-colors screen)) (color-map-norm (screen-colors screen))))
-      (#\b ; bright off
-       (setf l 1
-	     (color-map-current (screen-colors screen)) (color-map-norm (screen-colors screen))
-	     (xlib:gcontext-foreground gc) (get-fg-color screen (color-fg (screen-colors screen)))
-	     (xlib:gcontext-background gc) (get-bg-color screen (color-bg (screen-colors screen))))
-       (return-from set-color l))
-      (#\B ; bright on
-       (setf l 1
-	     (color-map-current (screen-colors screen)) (color-map-bright (screen-colors screen))
-	     (xlib:gcontext-foreground gc) (get-fg-color screen (color-fg (screen-colors screen)))
-	     (xlib:gcontext-background gc) (get-bg-color screen (color-bg (screen-colors screen))))
-       (return-from set-color l)))
-    (let ((fg (if (equal f "*") (get-fg-color-pixel screen) (get-fg-color screen (parse-integer f))))
-	  (bg (if (equal b "*") (get-bg-color-pixel screen) (get-bg-color screen (parse-integer b)))))
-      (setf (xlib:gcontext-foreground gc) fg
-	    (xlib:gcontext-background gc) bg)
-      l)))
+  (let* ((l (- (length s) i))
+	 (r 2)
+	 (f (subseq s i (1+ i)))
+	 (b (if (< l 2) "*" (subseq s (1+ i) (+ i 2)))))
+    (labels ((update-colors ()
+			    (setf
+			      (xlib:gcontext-foreground gc) (get-fg-color screen (color-fg (screen-colors screen)))
+			      (xlib:gcontext-background gc) (get-bg-color screen (color-bg (screen-colors screen))))))
+      (case (elt f 0)
+	(#\n ; normal
+	 (setf f "*" b "*" r 1
+	       (color-map-current (screen-colors screen)) (color-map-norm (screen-colors screen)))
+	 (get-fg-color screen nil)
+	 (get-bg-color screen nil))
+	(#\b ; bright off
+	 (setf r 1
+	       (color-map-current (screen-colors screen)) (color-map-norm (screen-colors screen)))
+	 (update-colors)
+	 (return-from set-color r))
+	(#\B ; bright on
+	 (setf r 1
+	       (color-map-current (screen-colors screen)) (color-map-bright (screen-colors screen)))
+	 (update-colors)
+	 (return-from set-color r))
+	(#\^ ; circumflex
+	 (return-from set-color 1)))
+      (let ((fg (if (equal f "*") (progn (get-fg-color screen nil) (get-fg-color-pixel screen)) (get-fg-color screen (parse-integer f))))
+	    (bg (if (equal b "*") (progn (get-bg-color screen nil) (get-bg-color-pixel screen)) (get-bg-color screen (parse-integer b)))))
+	(setf (xlib:gcontext-foreground gc) fg
+	      (xlib:gcontext-background gc) bg)
+	r))))
 
 (defun init-screen (screen-number id host)
   "Given a screen number, returns a screen structure with initialized members"
