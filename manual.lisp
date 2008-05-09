stumpwm/stumpwm

commit 5fe70595c3fecee59b181724e906d2a81be84573

Fixed manual generation with clisp.
Now it shouldn't crash anymore but the generated documentation lacks functions and commands arguments list.

Makefile.in
@@ -1,9 +1,11 @@
 LISP=@LISP_PROGRAM@
 
-clisp_OPTS=-K full -on-error exit ./make-image.lisp
-sbcl_OPTS=--load ./make-image.lisp
+clisp_BUILDOPTS=-K full -on-error exit ./make-image.lisp
+sbcl_BUILDOPTS=--load ./make-image.lisp
 
-LISP_OPTS= $(@LISP@_OPTS)
+clisp_INFOOPTS=-K full -on-error exit -x "(asdf:operate 'asdf:load-op :stumpwm) (load \"manual.lisp\")(stumpwm::generate-manual)(ext:exit)"
+sbcl_INFOOPTS=--eval "(progn (require 'asdf) (require 'stumpwm) (load \"manual.lisp\"))" --eval "(progn (stumpwm::generate-manual) (sb-ext:quit))"
+
 
 # You shouldn't have to edit past this
 
@@ -18,10 +20,10 @@ stumpwm.info: stumpwm.texi
 
 # FIXME: This rule is too hardcoded 
 stumpwm.texi: stumpwm.texi.in
-	sbcl --eval "(progn (require 'asdf) (require 'stumpwm) (load \"manual.lisp\"))" --eval "(progn (stumpwm::generate-manual) (sb-ext:quit))"
+	$(LISP) $(@LISP@_INFOOPTS)
 
 stumpwm: $(FILES)
-	$(LISP) $(LISP_OPTS)
+	$(LISP) $(@LISP@_BUILDOPTS)
 
 release:
 	git-archive --format=tar --prefix=stumpwm-@PACKAGE_VERSION@/ HEAD > stumpwm-@PACKAGE_VERSION@.tar

manual.lisp
@@ -35,9 +35,12 @@
 						(fdefinition (read-from-string name))))
 					    (symbol-function (find-symbol (string-upcase name) :stumpwm))))
 				    (*print-pretty* nil))
-				(format s "@defun {~a} ~{~a~^ ~}~%~a~&@end defun~%~%"
-					name (sb-impl::%simple-fun-arglist fn)
-					(documentation fn 'function))
+				#+sbcl (format s "@defun {~a} ~{~a~^ ~}~%~a~&@end defun~%~%"
+					       name (sb-impl::%simple-fun-arglist fn)
+					       (documentation fn 'function))
+				#-sbcl (format s "@defun {~a} ~%~a~&@end defun~%~%"
+					       name ;(sb-impl::%simple-fun-arglist fn)
+					       (documentation fn 'function))
 				t)))
 
 (defun generate-macro-doc (s line)
@@ -60,9 +63,12 @@
 (defun generate-command-doc (s line)
   (ppcre:register-groups-bind (name) ("^!!! (.*)" line)
 			      (let ((cmd (symbol-function (find-symbol (string-upcase name) :stumpwm))))
-				(format s "@deffn {Command} ~a ~{~a~^ ~}~%~a~&@end deffn~%~%"
-					name (sb-impl::%simple-fun-arglist cmd)
-                                        (documentation cmd 'function))
+				#+sbcl (format s "@deffn {Command} ~a ~{~a~^ ~}~%~a~&@end deffn~%~%"
+					       name (sb-impl::%simple-fun-arglist cmd)
+					       (documentation cmd 'function))
+				#-sbcl (format s "@deffn {Command} ~a ~%~a~&@end deffn~%~%"
+					       name ;(sb-impl::%simple-fun-arglist cmd)
+					       (documentation cmd 'function))
 				t)))
 
 (defun generate-manual (&key (in #p"stumpwm.texi.in") (out #p"stumpwm.texi"))

commit d6e1d86f383335f3b3bd9af94c61f30090634a82

fix some bugs introduced in 5fe70595c3fecee59b181724e906d2a81be84573 and b74f6114470598e080caccd42d0620eaa91fde70
* for generating the manual with clisp, stumpwm were not being loaded properly.
* compile manual.lisp before loading it so it isn't painfully slow
* remove a spurious #+sbcl from manual.lisp
* put the { } back around function names

Makefile.in
@@ -3,7 +3,7 @@ LISP=@LISP_PROGRAM@
 clisp_BUILDOPTS=-K full -on-error exit ./make-image.lisp
 sbcl_BUILDOPTS=--load ./make-image.lisp
 
-clisp_INFOOPTS=-K full -on-error exit -x "(asdf:operate 'asdf:load-op :stumpwm) (load \"manual.lisp\")(stumpwm::generate-manual)(ext:exit)"
+clisp_INFOOPTS=-K full -on-error exit -x "(load \"asdf.lisp\") (load \"stumpwm.asd\") (load \"@PPCRE_PATH@/cl-ppcre.asd\") (asdf:operate 'asdf:load-op :stumpwm) (load (compile-file \"manual.lisp\")) (stumpwm::generate-manual) (ext:exit)"
 sbcl_INFOOPTS=--eval "(progn (require 'asdf) (require 'stumpwm) (load \"manual.lisp\"))" --eval "(progn (stumpwm::generate-manual) (sb-ext:quit))"
 
 
@@ -11,7 +11,7 @@ sbcl_INFOOPTS=--eval "(progn (require 'asdf) (require 'stumpwm) (load \"manual.l
 
 # This is copied from the .asd file. It'd be nice to have the list in
 # one place, but oh well.
-FILES=package.lisp primitives.lisp keysyms.lisp keytrans.lisp kmap.lisp input.lisp core.lisp user.lisp mode-line.lisp color.lisp stumpwm.lisp version.lisp make-image.lisp
+FILES=package.lisp primitives.lisp wrappers.lisp keysyms.lisp keytrans.lisp kmap.lisp input.lisp core.lisp screen.lisp group.lisp frame.lisp window.lisp message-window.lisp events.lisp selection.lisp user.lisp fdump.lisp mode-line.lisp color.lisp stumpwm.lisp version.lisp
 
 all: stumpwm.info stumpwm
 

manual.lisp
@@ -35,7 +35,7 @@
                                                 (fdefinition (read-from-string name))))
                                             (symbol-function (find-symbol (string-upcase name) :stumpwm))))
                                     (*print-pretty* nil))
-                                (format s "@defun ~a ~{~a~^ ~}~%~a~&@end defun~%~%"
+                                (format s "@defun {~a} ~{~a~^ ~}~%~a~&@end defun~%~%"
                                         name
                                         #+sbcl (sb-impl::%simple-fun-arglist fn)
                                         #+clisp (ext:arglist fn)
@@ -63,12 +63,12 @@
 (defun generate-command-doc (s line)
   (ppcre:register-groups-bind (name) ("^!!! (.*)" line)
                               (let ((cmd (symbol-function (find-symbol (string-upcase name) :stumpwm))))
-                                #+sbcl (format s "@deffn {Command} ~a ~{~a~^ ~}~%~a~&@end deffn~%~%"
-                                               name
-                                               #+sbcl (sb-impl::%simple-fun-arglist cmd)
-                                               #+clisp (ext:arglist cmd)
-                                               #- (or sbcl clisp) '("(Check the code for args list)")
-                                               (documentation cmd 'function))
+                                (format s "@deffn {Command} ~a ~{~a~^ ~}~%~a~&@end deffn~%~%"
+                                        name
+                                        #+sbcl (sb-impl::%simple-fun-arglist cmd)
+                                        #+clisp (ext:arglist cmd)
+                                        #- (or sbcl clisp) '("(Check the code for args list)")
+                                        (documentation cmd 'function))
                                 t)))
 
 (defun generate-manual (&key (in #p"stumpwm.texi.in") (out #p"stumpwm.texi"))

stumpwm.texi
@@ -1226,7 +1226,7 @@ A command to toggle the mode line visibility.
 @end deffn
 
 
-@defun {toggle-mode-line} SCREEN HEAD &OPTIONAL (FORMAT (QUOTE *SCREEN-MODE-LINE-FORMAT*))
+@defun {toggle-mode-line} SCREEN HEAD &OPTIONAL (FORMAT '*SCREEN-MODE-LINE-FORMAT*)
 Toggle the state of the mode line for the specified screen
 @end defun
 