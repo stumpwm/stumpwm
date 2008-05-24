55f5777d85dd31b552cf66eef149a49cc07df518
Jonathan Moore Liles <wantingwaiting@users.sf.net>
2008-05-24 16:04:55 -0500

    Don't update mode-lines when focus changes.
    Doing so results in seemingly poor responsiveness with complex
    mode-line formats, specifically those which execute external commands.

--- window.lisp
+++ window.lisp
@@ -1077,7 +1077,6 @@ maximized, and given focus."
          (cw (screen-focus screen)))
     ;; If window to focus is already focused then our work is done.
     (unless (eq window cw)
-      (update-all-mode-lines)
       (raise-window window)
       (screen-set-focus screen window)
       ;;(send-client-message window :WM_PROTOCOLS +wm-take-focus+)
