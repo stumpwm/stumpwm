6488f0ca9720632c6b13c0bd6a8f06588ecc7f4a
Morgan Veyret <patzy@appart.kicks-ass.net>
2008-08-09 00:22:33 +0200

    Added support for automatic group creation and/or restoration.
    This adds three keyword parameters to define-frame-preference:
      * :create , when T the group matching the rule is created if it doesn't already exist.
        The group may be restored from a dump-file in *data-dir* if such a file exists (see :dump-name).
        Defaults to nil.
      * :restore , for group restoration from a dump file even if the matching group already exists.
        Defaults to nil.
      * :dump-name , group dump file to use for group restoration. File should be in *data-dir*.
        Defaults to group name.

--- window.lisp
+++ window.lisp
@@ -799,8 +799,10 @@ than the root window's width and height."
 
 (defun window-matches-rule-p (w rule)
   "Returns T if window matches rule"
-  (destructuring-bind (group-name frame raise lock &rest props) rule
-    (declare (ignore frame raise))
+  (destructuring-bind (group-name frame raise lock create restore
+                                  dump-name
+                                  &rest props) rule
+    (declare (ignore frame raise create restore dump-name))
     (if (or lock
             (equal group-name (group-name (or (window-group w) (current-group)))))
         (apply 'window-matches-properties-p w props))))
@@ -816,14 +818,34 @@ than the root window's width and height."
   the window should be raised."
   (let ((match (rule-matching-window window)))
     (if match
-        (destructuring-bind (group-name frame raise lock &rest props) match
+        (destructuring-bind (group-name frame raise lock
+                             create restore dump-name
+                             &rest props) match
           (declare (ignore lock props))
           (let ((group (find-group screen group-name)))
-            (if group
-                (values group (frame-by-number group frame) raise)
-                (progn
-                  (message "^B^1*Error placing window, group \"^b~a^B\" does not exist." group-name)
-                  (values)))))
+            (cond (group
+                   (when restore
+                     (if (probe-file dump-name)
+                         (progn (message "Restoring group \"^b~a^B\" from \"^b~a^B\"." group-name dump-name)
+                                (restore-group group
+                                               (read-dump-from-file dump-name)))
+                         (message "^B^1*Error restoring group \"^b~a^B\" from \"^b~a^B\"." group dump-name)))
+                   (values group (frame-by-number group frame) raise))
+                  (create
+                   (let ((new-group (add-group (current-screen) group-name)))
+                     (if (and new-group
+                              (probe-file dump-name))
+                         (progn (restore-group new-group
+                                               (read-dump-from-file dump-name))
+                                (values new-group
+                                        (frame-by-number new-group frame)
+                                        raise))
+                         (progn (message "^B^1*Error restoring group \"^b~a^B\" from \"^b~a^B\"." new-group dump-name)
+                                (values new-group
+                                        (frame-by-number new-group frame)
+                                        raise)))))
+                    (t (message "^B^1*Error placing window, group \"^b~a^B\" does not exist." group-name)
+                       (values)))))
         (values))))
 
 (defun sync-window-placement ()
@@ -983,7 +1005,7 @@ needed."
   (declare (type window window))
   ;; put it in a valid group
   (let ((screen (window-screen window)))
-    ;; Use window plaecment rules
+    ;; Use window placement rules
     (multiple-value-bind (group frame raise) (get-window-placement screen window)
       (declare (ignore raise))
       (unless (find (window-group window)
@@ -1440,7 +1462,8 @@ be used to override the default window formatting."
          (group-name (group-name group))
          (frame-number (frame-number (window-frame window)))
          (role (window-role window)))
-    (push (list group-name frame-number t lock
+    (push (list group-name frame-number t lock nil nil
+                (concat *data-dir* group-name)
                 :class (window-class window)
                 :instance (window-res window)
                 :title (and title (window-name window))
