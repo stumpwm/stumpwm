359c994035c08aeae1f061ef5eecd6052ad193e8
Shawn <sabetts@juicebox>
2008-10-03 14:14:40 -0700

    only catch errors in eval-line

--- user.lisp
+++ user.lisp
@@ -297,7 +297,7 @@ such a case, kill the shell command to resume StumpWM."
       (message "^20~{~a~^~%~}"
                (mapcar 'prin1-to-string
                        (multiple-value-list (eval (read-from-string cmd)))))
-    (t (c)
+    (error (c)
       (err "^B^1*~A" c))))
 
 (defcommand-alias eval eval-line)
