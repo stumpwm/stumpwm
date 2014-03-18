f268ae3d904aa169d2b0df5eaa896561559d9d5b
Michael <@>
2014-03-18 17:21:45 +0400

    Export one more function

--- group.lisp
+++ group.lisp
@@ -24,7 +24,7 @@
 
 (in-package #:stumpwm)
 
-(export '(current-group group-windows))
+(export '(current-group group-windows move-window-to-group))
 
 (defvar *default-group-type* 'tile-group
   "The type of group that should be created by default.")
