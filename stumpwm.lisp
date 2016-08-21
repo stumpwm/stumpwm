4ea96e4b998e438d27bcad75918e3bc039bddd52
Fredrik Tolf <fredrik@dolda2000.com>
2016-08-21 21:19:11 +0200

    Extract top-level error handling to a generic function.
    This also changes so that non-warning, non-serious conditions are
    simply ignored, which should be proper beavior.

--- stumpwm.lisp
+++ stumpwm.lisp
@@ -135,7 +135,17 @@ The action is to call FUNCTION with arguments ARGS."
             internal-time-units-per-second)
          0)))
 
-(defun perform-top-level-error-action (c)
+(defgeneric handle-top-level-condition (c))
+
+(defmethod handle-top-level-condition (c)
+  ;; Do nothing by default; there's nothing wrong with signalling
+  ;; arbitrary conditions
+  )
+
+(defmethod handle-top-level-condition ((c warning))
+  (muffle-warning))
+
+(defmethod handle-top-level-condition ((c serious-condition))
   (ecase *top-level-error-action*
     (:message
      (let ((s (format nil "~&Caught '~a' at the top level. Please report this." c)))
@@ -145,6 +155,11 @@ The action is to call FUNCTION with arguments ARGS."
     (:break (invoke-debugger c))
     (:abort
      (throw :top-level (list c (backtrace-string))))))
+
+(defmethod handle-top-level-condition ((c xlib:lookup-error))
+  (if (lookup-error-recoverable-p)
+      (recover-from-lookup-error)
+      (call-next-method)))
 
 (defclass stumpwm-timer-channel () ())
 
@@ -191,25 +206,8 @@ The action is to call FUNCTION with arguments ARGS."
          (setf *toplevel-io* io)
          (loop
             (handler-bind
-                ((xlib:lookup-error (lambda (c)
-                                      (if (lookup-error-recoverable-p)
-                                          (recover-from-lookup-error)
-                                          (error c))))
-                 (warning #'muffle-warning)
-                 ((or serious-condition error)
-                  (lambda (c)
-                    (run-hook *top-level-error-hook*)
-                    (perform-top-level-error-action c)))
-                 (t
-                  (lambda (c)
-                    ;; some other wacko condition was raised so first try
-                    ;; what we can to keep going.
-                    (cond ((find-restart 'muffle-warning)
-                           (muffle-warning))
-                          ((find-restart 'continue)
-                           (continue)))
-                    ;; and if that fails treat it like a top level error.
-                    (perform-top-level-error-action c))))
+                ((t (lambda (c)
+                      (handle-top-level-condition c))))
               (io-loop io :description "StumpWM")))))))
 
 (defun parse-display-string (display)
