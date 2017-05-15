698621aba07485f18e91aca1da246e81e40ffdcb
Thomas <thomas@pinegrove.io>
2017-05-15 10:59:25 +1000

    Remove other compilers from directory-no-deref

--- wrappers.lisp
+++ wrappers.lisp
@@ -123,12 +123,7 @@
 
 (defun directory-no-deref (pathspec)
   "Call directory without dereferencing symlinks in the results"
-  #+(or cmu scl) (directory pathspec :truenamep nil)
-  #+clisp (mapcar #'car (directory pathspec :full t))
-  #+lispworks (directory pathspec :link-transparency nil)
-  #+openmcl (directory pathspec :follow-links nil)
-  #+sbcl (directory pathspec :resolve-symlinks nil)
-  #-(or clisp cmu lispworks openmcl sbcl scl) (directory pathspec))
+  (directory pathspec :resolve-symlinks nil))
 
 ;;; CLISP does not include features to distinguish different Unix
 ;;; flavours (at least until version 2.46). Until this is fixed, use a
