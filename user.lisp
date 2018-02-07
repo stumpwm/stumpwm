b38457432c8867de68a390fd52161d4caae6276e
Moch Deden <selesdepselesnul@gmail.com>
2018-02-07 20:43:17 +0700

    Add newline

--- user.lisp
+++ user.lisp
@@ -195,7 +195,8 @@ such a case, kill the shell command to resume StumpWM."
 
 (defcommand loadrc () ()
 "Reload the @file{~/.stumpwmrc} file."
-  (handler-case (with-restarts-menu (load-rc-file nil))
+  (handler-case 
+    (with-restarts-menu (load-rc-file nil))
     (error (c)
       (message "^1*^BError loading rc file: ^n~A" c))
     (:no-error (&rest args)
