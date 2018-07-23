161b759e2a6bde3c89d1d71f00dbe5e1117515d7
David Bjergaard <dbjergaard@users.noreply.github.com>
2018-07-23 12:28:12 -0400

    Wordwrap docstring

--- kmap.lisp
+++ kmap.lisp
@@ -38,7 +38,9 @@
 @dfn{prefix map}.")
 
 (defvar *root-map* nil
-  "This is the keymap by default bound to @kbd{C-t} (along with *group-root-map* and either *tile-group-root-map* or *float-group-root-map*). It is known as the @dfn{prefix map}.")
+  "This is the keymap by default bound to @kbd{C-t} (along with 
+ *group-root-map* and either *tile-group-root-map* or 
+ *float-group-root-map*). It is known as the @dfn{prefix map}.")
 
 (defstruct key
   keysym shift control meta alt hyper super)
