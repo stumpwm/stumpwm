ef338d5f070c0b887d3a1216e73dab7ed2841ce6
Shozo <39150985+szos@users.noreply.github.com>
2018-10-11 13:15:09 -0700

    Update interactive-keymap.lisp
    Changed implementation to avoid adding another macro to the package.

--- interactive-keymap.lisp
+++ interactive-keymap.lisp
@@ -36,7 +36,10 @@
   (message "~S finished" name)
   (pop-top-map))
 
-(defmacro define-interactive-keymap (name (&key on-enter on-exit abort-if) &body key-bindings)
+(defmacro define-interactive-keymap (name (&key on-enter on-exit abort-if
+                                                (exit-on '((kbd "RET") (kbd "ESC")
+                                                           (kbd "C-g"))))
+                                          &body key-bindings)
   "Declare an interactive keymap mode. This can be used for developing
 interactive modes or command trees, such as @command{iresize}.
 
@@ -58,35 +61,9 @@ Be aware that these commands won't require a prefix to run."
       `(let ((,keymap (make-sparse-keymap)))
          ,@(loop for keyb in key-bindings
                  collect `(define-key ,keymap ,@keyb))
-         (define-key ,keymap (kbd "RET") ,exit-command)
-         (define-key ,keymap (kbd "C-g") ,exit-command)
-         (define-key ,keymap (kbd "ESC") ,exit-command)
+         ,@(loop for keyb in exit-on
+                 collect `(define-key ,keymap ,keyb ,exit-command))
 
-         (defcommand ,name () ()
-           ,@decls
-           ,(or docstring
-                (format nil "Starts interactive command \"~A\"" command))
-           ,@(when abort-if `((when (funcall ,abort-if)
-                                (return-from ,command))))
-
-           ,@(when on-enter `((funcall ,on-enter)))
-           (enter-interactive-keymap ,keymap (quote ,command)))
-
-         (defcommand ,(intern exit-command) () ()
-           ,@(when on-exit `((funcall ,on-exit)))
-           (exit-interactive-keymap (quote ,command)))))))
-
-
-(defmacro define-interactive-keymap-no-return (name (&key on-enter on-exit abort-if) &body key-bindings)
-  "Same as DEFINE-INTERACTIVE-KEYMAP, except no keys are bound to exit-command"
-  (let* ((command (if (listp name) (car name) name))
-         (exit-command (format nil "EXIT-~A" command))
-         (keymap (gensym "m")))
-    (multiple-value-bind (key-bindings decls docstring)
-        (parse-body key-bindings :documentation t)
-      `(let ((,keymap (make-sparse-keymap)))
-         ,@(loop for keyb in key-bindings
-                 collect `(define-key ,keymap ,@keyb))
          (defcommand ,name () ()
            ,@decls
            ,(or docstring
