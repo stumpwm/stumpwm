stumpwm/stumpwm

commit 12c9202df4ecb767c220619e0b20a1170bc6b6bc

Update focus when clicking a TILE-WINDOW  behind a FLOAT-WINDOW
Previously is a floating window was 'before' a tiled window clicking
the tiled window would not focus the window. This was because the tile
window wasn't listed by TOP-WINDOWS because of an error in
WINDOW-VISIBLE-P. Fix WINDOW-VISIBLE-P so that it takes into account the
case where the tiled window is below a floating window.

Closes #548

tile-window.lisp
@@ -37,11 +37,20 @@ like xterm and emacs.")
      (maximize-window window))))
 
 (defmethod window-visible-p ((window tile-window))
-  ;; In this case, visible means the window is the top window in the
-  ;; frame. This is not entirely true when it doesn't take up the
-  ;; entire frame and there's a window below it.
-  (eq window
-      (frame-window (window-frame window))))
+  ;; A TILE-WINDOW is visible is it is the top window in the frame or when the
+  ;; focused window is a FLOAT-WINDOW and the TILE-WINDOW can be seen below.
+  (let* ((frame (window-frame window))
+         (frame-windows (frame-windows (window-group window) frame)))
+    (flet ((full-frame-p (win)
+             (not (and (xlib:wm-size-hints-x (window-normal-hints win))
+                       (xlib:wm-size-hints-y (window-normal-hints win))))))
+      (or (eq window (or (frame-window frame)
+                         (first frame-windows)))
+          (when (> (length frame-windows) 1)
+            (loop :for current-window :in frame-windows
+                  :until (full-frame-p current-window)
+                  :when (eq window current-window)
+                    :do (return t)))))))
 
 (defmethod window-head ((window tile-window))
   (frame-head (window-group window) (window-frame window)))

AUTHORS
@@ -71,3 +71,4 @@ Stuart Dilts        stuart.dilts at gmail com
 Ram Krishnan        kriyative at gmail.com
 Herbert Jones       jones dot herbert at gmail.com
 Daniel Oliveira     drdo at drdo.eu
+Panji Kusuma        epanji at gmail dot com

commit 47afa9a6e51fd28a1bdd501ec7bcd7c9485087a6

Fix bug introduced in 12c9202
When modifying the coding style of PR #548 the intended logic was
modified. Restore the original intended behaviour.

tile-window.lisp
@@ -47,9 +47,9 @@ like xterm and emacs.")
       (or (eq window (or (frame-window frame)
                          (first frame-windows)))
           (when (> (length frame-windows) 1)
-            (loop :for current-window :in frame-windows
+            (loop :for (current-window next-window) :on frame-windows
                   :until (full-frame-p current-window)
-                  :when (eq window current-window)
+                  :when (eq window next-window)
                     :do (return t)))))))
 
 (defmethod window-head ((window tile-window))