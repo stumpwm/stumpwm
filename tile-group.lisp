151fdf45239d53977e47dd3dc95be4db02bdd534
Shozo <39150985+szos@users.noreply.github.com>
2019-02-06 13:34:10 -0800

    add command 'remove-sibling'
    adding the command remove-sibling, which recursively removes the sibling* frame of the current frame. I'm not sure if this functionality exists, but I couldn't find it. 
    
    *sibling here refers to whats on the other side of a split, not the frame focused by the sibling command (which seems to always be the 0th frame)

--- tile-group.lisp
+++ tile-group.lisp
@@ -1052,6 +1052,39 @@ space."
             (show-frame-indicator group))
           (run-hook-with-args *remove-split-hook* l frame)))))
 
+(defcommand (remove-sibling tile-group)
+    (&optional (group (current-group))
+	       (frame (let ((window-type (type-of (current-window))))
+			           (if (eq window-type 'float-window)
+			               nil ;; (focus-all (current-window))
+			               (window-frame (current-window))))))
+    ()
+  "removes the closest sibling frame to the specified frame, which defaults
+to the current frame. if the closes sibling is split, we call 
+remove-split-sibling, otherwise we just remove the sibling frame. "
+  (when frame
+    (labels ((remove-split-sibling (tree group)
+	       (let ((left (car tree))
+		     (right (second tree)))
+		 (when (listp left)
+		   (remove-split-sibling left group))
+		 (when (listp right)
+		   (remove-split-sibling right group))
+		 (when (frame-p left)
+		   (remove-split group left))
+		 (when (frame-p right)
+		   (remove-split group right)))))
+      (let ((sibling
+	     (closest-sibling
+	      (list (tile-group-frame-head group (frame-head group frame)))
+	      frame)))
+	     (cond ((listp sibling) ;; sibling is split
+	            (remove-split-sibling sibling group))
+	           ((frame-p sibling) ;; sibling is a frame
+	            (remove-split group sibling))
+	           (t ;; something went wrong, show the current frame.
+	            (curframe)))))))
+
 (defcommand-alias remove remove-split)
 
 (defcommand (only tile-group) () ()
