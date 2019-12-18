51b378ab12aad54dbc25471faf4a90bc5d63ecea
Spenser Truex <web@spensertruex.com>
2019-12-18 14:02:44 -0800

    Use implicit dispatch to generate functions
    - This makes adding a new defdoc sufficient to add a new line.

--- manual.lisp
+++ manual.lisp
@@ -29,6 +29,9 @@
 (defvar *debug-doc* nil
   "When T, will print extra debugging information from the doc generator.")
 
+(defvar *valid-doctypes* nil
+  "All the line-types that can be generated for the StumpWM documentation.")
+
 (defun dprint (type sym)
   "Handy for figuring out which symbol is borking the documentation."
   (declare (ignorable type sym))
@@ -42,17 +45,23 @@
                          "@" new-name " " body-format "~&@end " new-name "~%~%")
            ,@args))
 
-(defmacro defdoc (name ((out-stream-var line-var name-var)
-                        marker dprint-label)
-                  &body body)
-  "Define a document generating function."
-  `(defun ,name (,out-stream-var ,line-var)
-     (ppcre:register-groups-bind (,name-var)
-         (,(format nil "~@{~A~}" "^" marker "\\W(.*)") ,line-var)
-       (dprint ',dprint-label ,name-var)
-       ,@body)))
+(defgeneric generate (specializer out-stream line)
+  (:documentation "Generate a texi.in documentation line."))
 
-(defdoc generate-function-doc ((s line name) "@@@" func)
+(defmacro defdoc ((specializer
+                   (out-stream-var line-var name-var)
+                   marker dprint-label)
+                  &body body)
+  "Define a document generating method."
+  `(progn
+     (pushnew ,specializer *valid-doctypes*)
+     (defmethod generate ((,(gensym) (eql ,specializer)) ,out-stream-var ,line-var)
+       (ppcre:register-groups-bind (,name-var)
+           (,(format nil "~@{~A~}" "^" marker "\\W(.*)") ,line-var)
+         (dprint ',dprint-label ,name-var)
+         ,@body))))
+
+(defdoc (:function (s line name) "@@@" func)
   (let ((fn (if (find #\( name :test 'char=)
                 ;; handle (setf <symbol>) functions
                 (with-standard-io-syntax
@@ -65,7 +74,7 @@
         (documentation fn 'function)))
     t))
 
-(defdoc generate-macro-doc ((s line name) "%%%" macro)
+(defdoc (:macro (s line name) "%%%" macro)
   (let* ((symbol (find-symbol (string-upcase name) :stumpwm)))
     (let ((*print-pretty* nil))
       (doc-fmt s "defmac" "{~a} ~{~a~^ ~}~%~a"
@@ -74,19 +83,19 @@
         (documentation symbol 'function)))
     t))
 
-(defdoc generate-variable-doc ((s line name) "###" var)
+(defdoc (:variable (s line name) "###" var)
   (let ((sym (find-symbol (string-upcase name) :stumpwm)))
     (doc-fmt s "defvar" "~a~%~a"
       name (documentation sym 'variable))
     t))
 
-(defdoc generate-hook-doc ((s line name) "$$$" hook)
+(defdoc (:hook (s line name) "$$$" hook)
   (let ((sym (find-symbol (string-upcase name) :stumpwm)))
     (doc-fmt s "defvr" "{Hook} ~a~%~a"
       name (documentation sym 'variable))
     t))
 
-(defdoc generate-command-doc ((s line name) "!!!" cmd)
+(defdoc (:command (s line name) "!!!" cmd)
   (if-let (symbol (find-symbol (string-upcase name) :stumpwm))
     (let ((cmd (symbol-function symbol))
           (*print-pretty* nil))
@@ -97,16 +106,17 @@
       t)
     (warn "Symbol ~A not found in package STUMPWM" name)))
 
+(defmethod generate ((specializer null) os line)
+  (mapc (lambda (spec)
+          (when (generate spec os line)
+            (return-from generate)))
+        *valid-doctypes*)
+  (write-line line os))
+
 (defun generate-manual (&key (in #p"stumpwm.texi.in") (out #p"stumpwm.texi"))
   "Generate the texinfo manual from the template texi.in file."
   (let ((*print-case* :downcase))
     (with-open-file (os out :direction :output :if-exists :supersede)
       (with-open-file (is in :direction :input)
         (loop for line = (read-line is nil is)
-              until (eq line is) do
-              (or (generate-function-doc os line)
-                  (generate-macro-doc os line)
-                  (generate-hook-doc os line)
-                  (generate-variable-doc os line)
-                  (generate-command-doc os line)
-                  (write-line line os)))))))
+              until (eq line is) do (generate nil os line))))))
