a804b2f1bcad4a1bc8d159325f0b581d82287236
Spenser Truex <web@spensertruex.com>
2020-03-09 15:00:01 -0700

    Use ASDF UIOP's xdg base dir functions
    Form pathnames that are valid XDG Base Dir locations according to the
    specification.

--- stumpwm.lisp
+++ stumpwm.lisp
@@ -39,17 +39,12 @@ doesn't exist. Returns a values list: whether the file loaded (t if no
 rc files exist), the error if it didn't, and the rc file that was
 loaded. When CATCH-ERRORS is nil, errors are left to be handled
 further up. "
-  (let* ((xdg-config-dir
-           (let ((dir (getenv "XDG_CONFIG_HOME")))
-             (if (or (not dir) (string= dir ""))
-                 (merge-pathnames  #p".config/" (user-homedir-pathname))
-                 (concatenate 'string dir "/"))))
-         (user-rc
+  (let* ((user-rc
            (probe-file (merge-pathnames #p".stumpwmrc" (user-homedir-pathname))))
          (dir-rc
            (probe-file (merge-pathnames #p".stumpwm.d/init.lisp" (user-homedir-pathname))))
          (conf-rc
-           (probe-file (merge-pathnames #p"stumpwm/config" xdg-config-dir)))
+           (probe-file (uiop:xdg-config-home #p"stumpwm/config/")))
          (etc-rc (probe-file #p"/etc/stumpwmrc"))
          (rc (or user-rc dir-rc conf-rc etc-rc)))
     (if rc
