ba4a9754f0dbaa748b342a7820352c14a8e90202
jcguu95 <jcguu95@gmail.com>
2021-06-06 19:02:46 +0800

    Minor: fix indentation.

--- dynamic-floating-group.lisp
+++ dynamic-floating-group.lisp
@@ -97,36 +97,36 @@ information will be used when the group re-tiles it."
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (symbol-macrolet ((dyn-order (dyn-float-group-dyn-order group)))
-        ;; If window W does not have a corresponding W+ in the
-        ;; dyn-order, make one for it.
-        (loop for w in (stumpwm::group-windows group)
-              do (unless (member w (mapcar #'window+-window dyn-order))
-                   (push (make-window+ :window w :free nil) dyn-order)))
-        ;; If window W+ does not correspond to a window of GROUP,
-        ;; delete W+ from the dyn-order.
-        (loop for w+ in dyn-order
-              do (unless (member (window+-window w+) (stumpwm::group-windows group))
-                   (alexandria:deletef dyn-order w+)))
-        ;; Make the free windows on top of the stack.
-        (setf dyn-order
-              (append (remove-if (lambda (dyno)
-                                   (equal nil (window+-free dyno)))
-                                 dyn-order)
-                      (remove-if (lambda (dyno)
-                                   (equal t (window+-free dyno)))
-                                 dyn-order)))
-        ;; Let the (group-windows group) respect the order of
-        ;; dyn-order
-        (setf (stumpwm::group-windows group)
-              (mapcar #'window+-window dyn-order))))
+    ;; If window W does not have a corresponding W+ in the
+    ;; dyn-order, make one for it.
+    (loop for w in (stumpwm::group-windows group)
+          do (unless (member w (mapcar #'window+-window dyn-order))
+               (push (make-window+ :window w :free nil) dyn-order)))
+    ;; If window W+ does not correspond to a window of GROUP,
+    ;; delete W+ from the dyn-order.
+    (loop for w+ in dyn-order
+          do (unless (member (window+-window w+) (stumpwm::group-windows group))
+               (alexandria:deletef dyn-order w+)))
+    ;; Make the free windows on top of the stack.
+    (setf dyn-order
+          (append (remove-if (lambda (dyno)
+                               (equal nil (window+-free dyno)))
+                             dyn-order)
+                  (remove-if (lambda (dyno)
+                               (equal t (window+-free dyno)))
+                             dyn-order)))
+    ;; Let the (group-windows group) respect the order of
+    ;; dyn-order
+    (setf (stumpwm::group-windows group)
+          (mapcar #'window+-window dyn-order))))
 
 (defun current-window+ (&optional (group (stumpwm:current-group)))
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (let ((gcw (stumpwm::group-current-window group)))
-        (find-if (lambda (x)
-                   (equal gcw (window+-window x)))
-                 (dyn-float-group-dyn-order group))))
+    (find-if (lambda (x)
+               (equal gcw (window+-window x)))
+             (dyn-float-group-dyn-order group))))
 
 (defun next-window+ (&optional (N 1)
                        (group (stumpwm:current-group))
@@ -134,15 +134,15 @@ information will be used when the group re-tiles it."
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (let ((dyno (dyn-float-group-dyn-order group)))
-        (nth (mod (+ N (position window dyno))
-                  (length dyno))
-             dyno)))
+    (nth (mod (+ N (position window dyno))
+              (length dyno))
+         dyno)))
 
 (defcommand focus-next-window (&optional (N 1) (group (stumpwm:current-group))) ()
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (stumpwm::group-focus-window group
-                                   (window+-window (next-window+ N group))))
+                               (window+-window (next-window+ N group))))
 
 (defcommand focus-last-window (&optional (group (stumpwm:current-group))) ()
   (assert (dyn-float-group-p group) ()
@@ -153,28 +153,28 @@ information will be used when the group re-tiles it."
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (position (current-window+ group)
-                (dyn-float-group-dyn-order group)
-                :test #'equal))
+            (dyn-float-group-dyn-order group)
+            :test #'equal))
 
 (defun free-all (&optional (group (stumpwm:current-group)))
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   ;; alias: un-tile-all
-      (progn (loop for w+ in (dyn-float-group-dyn-order group)
-                   do (setf (window+-free w+) t))
-             (re-tile group)))
+  (progn (loop for w+ in (dyn-float-group-dyn-order group)
+               do (setf (window+-free w+) t))
+         (re-tile group)))
 
 ;; This will effectively force re-tile all windows in this group.
 (defcommand unfree-all
     (&optional (group (stumpwm:current-group)))
-    ()
+  ()
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (progn
-        ;; alias: tile-all
-        (loop for w+ in (dyn-float-group-dyn-order group)
-              do (setf (window+-free w+) nil))
-        (re-tile group)))
+    ;; alias: tile-all
+    (loop for w+ in (dyn-float-group-dyn-order group)
+          do (setf (window+-free w+) nil))
+    (re-tile group)))
 
 (defun free-window (&optional
                       (window (stumpwm:current-window))
@@ -182,29 +182,29 @@ information will be used when the group re-tiles it."
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (progn
-        (loop for w+ in (dyn-float-group-dyn-order group)
-              if (equal window (window+-window w+))
-                do (setf (window+-free w+) t))
-        (re-tile group)))
+    (loop for w+ in (dyn-float-group-dyn-order group)
+          if (equal window (window+-window w+))
+            do (setf (window+-free w+) t))
+    (re-tile group)))
 
 (defcommand unfree-window
     (&optional
      (window (stumpwm:current-window))
      (group (stumpwm:current-group)))
-    ()
+  ()
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (progn
-        (symbol-macrolet ((dyno (dyn-float-group-dyn-order group)))
-          (loop for w+ in dyno
-                do (when (equal window (window+-window w+))
-                     (progn
-                       (alexandria:deletef dyno w+)
-                       (setf (window+-free w+) nil)
-                       (if (null dyno)
-                           (setf dyno (list w+))
-                           (push w+ (cdr (last dyno))))))))
-        (re-tile group)))
+    (symbol-macrolet ((dyno (dyn-float-group-dyn-order group)))
+      (loop for w+ in dyno
+            do (when (equal window (window+-window w+))
+                 (progn
+                   (alexandria:deletef dyno w+)
+                   (setf (window+-free w+) nil)
+                   (if (null dyno)
+                       (setf dyno (list w+))
+                       (push w+ (cdr (last dyno))))))))
+    (re-tile group)))
 
 (defun toggle-freeness-current-window
     (&optional
@@ -213,125 +213,125 @@ information will be used when the group re-tiles it."
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (progn
-        (if (eq (window+-free (current-window+ group)) t)
-            (unfree-window window group)
-            (free-window window group))
-        (re-tile group)))
+    (if (eq (window+-free (current-window+ group)) t)
+        (unfree-window window group)
+        (free-window window group))
+    (re-tile group)))
 
 (defun unfloating-windows+ (&optional (group (stumpwm:current-group)))
   "Return the list of window+s whose :FREE slot is nil."
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (remove-if-not
-       (lambda (w+)
-         (eq (window+-free w+) nil))
-       (dyn-float-group-dyn-order group)))
+   (lambda (w+)
+     (eq (window+-free w+) nil))
+   (dyn-float-group-dyn-order group)))
 
 (defun re-tile (&optional (group (stumpwm:current-group)))
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (progn
-        (sync-dyn-order group)
-        (let* ((cs (slot-value (stumpwm:current-screen) 'number))
-               (sw (xlib:screen-width cs))
-               (sh (xlib:screen-height cs))
-               (wl (mapcar #'window+-window (unfloating-windows+ group)))
-               (N (length wl))
+    (sync-dyn-order group)
+    (let* ((cs (slot-value (stumpwm:current-screen) 'number))
+           (sw (xlib:screen-width cs))
+           (sh (xlib:screen-height cs))
+           (wl (mapcar #'window+-window (unfloating-windows+ group)))
+           (N (length wl))
 
-               (master-ratio (dyn-float-group-master-ratio group))
-               (current-layout (car (dyn-float-group-layout-hist group))))
+           (master-ratio (dyn-float-group-master-ratio group))
+           (current-layout (car (dyn-float-group-layout-hist group))))
 
-          ;; Waiting for the fix for a related issue for general floating group.
-          ;; https://github.com/stumpwm/stumpwm/issues/864
-          (setf sw (- sw 2))  ; Adhoc hack to respect boarder width FIXME.
-          (setf sh (- sh 18)) ; Adhoc hack to respect modeline FIXME.
+      ;; Waiting for the fix for a related issue for general floating group.
+      ;; https://github.com/stumpwm/stumpwm/issues/864
+      (setf sw (- sw 2))  ; Adhoc hack to respect boarder width FIXME.
+      (setf sh (- sh 18)) ; Adhoc hack to respect modeline FIXME.
 
-          (case N
-            (0 nil)
-            (1 (stumpwm::float-window-move-resize
-                (car wl)
-                :x 0 :y 0 :width sw :height sh))
-            (t (case current-layout
-               ('left-vertical
-                (progn
-                  (stumpwm::float-window-move-resize
-                   (car wl)
-                   :x 0 :y 0 :width (round (* sw master-ratio)) :height sh)
-                  (loop for k from 1 to (- N 1)
-                        do (stumpwm::float-window-move-resize
-                            (nth k wl)
-                            :x (round (* sw master-ratio))
-                            :y (* (round (/ sh (- N 1))) (- k 1))
-                            :width (round (* sw (- 1 master-ratio)))
-                            :height (round (/ sh (- N 1)))))))
-               ('horizontal
-                (progn
-                  (stumpwm::float-window-move-resize
-                   (car wl)
-                   :x 0 :y 0 :width sw :height (round (* sh master-ratio)))
-                  (loop for k from 1 to (- N 1)
-                        do (stumpwm::float-window-move-resize
-                            (nth k wl)
-                            :x (* (round (/ sw (- N 1))) (- k 1))
-                            :y (round (* sh master-ratio))
-                            :width (round (/ sw (- N 1)))
-                            :height (round (* sh (- 1 master-ratio)))))))
-               ('fullscreen
-                (loop for k from 0 to (- N 1)
+      (case N
+        (0 nil)
+        (1 (stumpwm::float-window-move-resize
+            (car wl)
+            :x 0 :y 0 :width sw :height sh))
+        (t (case current-layout
+             ('left-vertical
+              (progn
+                (stumpwm::float-window-move-resize
+                 (car wl)
+                 :x 0 :y 0 :width (round (* sw master-ratio)) :height sh)
+                (loop for k from 1 to (- N 1)
                       do (stumpwm::float-window-move-resize
                           (nth k wl)
-                          :x 0 :y 0 :width sw :height sh)))
-               ;; ('right-vertical "TODO")
-               ;; ('fibonacci "TODO")
-               (otherwise (error "Layout isn't supported."))))))))
+                          :x (round (* sw master-ratio))
+                          :y (* (round (/ sh (- N 1))) (- k 1))
+                          :width (round (* sw (- 1 master-ratio)))
+                          :height (round (/ sh (- N 1)))))))
+             ('horizontal
+              (progn
+                (stumpwm::float-window-move-resize
+                 (car wl)
+                 :x 0 :y 0 :width sw :height (round (* sh master-ratio)))
+                (loop for k from 1 to (- N 1)
+                      do (stumpwm::float-window-move-resize
+                          (nth k wl)
+                          :x (* (round (/ sw (- N 1))) (- k 1))
+                          :y (round (* sh master-ratio))
+                          :width (round (/ sw (- N 1)))
+                          :height (round (* sh (- 1 master-ratio)))))))
+             ('fullscreen
+              (loop for k from 0 to (- N 1)
+                    do (stumpwm::float-window-move-resize
+                        (nth k wl)
+                        :x 0 :y 0 :width sw :height sh)))
+             ;; ('right-vertical "TODO")
+             ;; ('fibonacci "TODO")
+             (otherwise (error "Layout isn't supported."))))))))
 
 (defcommand rotate-window-list
     (&optional
      (group (stumpwm:current-group))
      opposite)
-    ()
+  ()
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (flet ((rotate-list (xs &optional opposite)
-               "An adhoc pure function that rotates the list."
-               (if opposite
-                   (append (cdr xs) (list (car xs)))
-                   (append (last xs) (butlast xs)))))
-        (symbol-macrolet ((dyno (dyn-float-group-dyn-order group)))
-          (setf dyno (rotate-list dyno opposite))
-          (re-tile group))))
+           "An adhoc pure function that rotates the list."
+           (if opposite
+               (append (cdr xs) (list (car xs)))
+               (append (last xs) (butlast xs)))))
+    (symbol-macrolet ((dyno (dyn-float-group-dyn-order group)))
+      (setf dyno (rotate-list dyno opposite))
+      (re-tile group))))
 
 (defcommand permute-window-list
     (&optional
      opposite
      (group (stumpwm:current-group))
      (n (current-window-position group)))
-    ()
+  ()
   (assert (dyn-float-group-p group) ()
           "Expected GROUP ~A to be of type DYN-FLOAT-GROUP." group)
   (flet ((permute-at (ring n)
-               "A pure function that permutes the nth and
+           "A pure function that permutes the nth and
 the (n+1)th element of RING."
-               ;; ((0 1 2 3 4 5) 3) => (0 1 2 4 3 5)
-               ;; ((0 1 2 3 4 5) 5) => (5 1 2 3 4 0)
-               (when (and (listp ring) (not (null ring)))
-                 (let* ((l (length ring))
-                        (n (mod n l)))
-                   (when (>= l 2)
-                     (if (= n (- l 1))
-                         (append (last ring)
-                                 (butlast (cdr ring))
-                                 (list (car ring)))
-                         (append (subseq ring 0 n)
-                                 (list (nth (mod (+ n 1) l) ring))
-                                 (list (nth (mod (+ n 0) l) ring))
-                                 (subseq ring (+ n 2)))))))))
-        (progn
-            (when opposite (setf n (- n 1)))
-            (symbol-macrolet
-                ((dyno (dyn-float-group-dyn-order group)))
-              (setf dyno (permute-at dyno n)))
-             (re-tile group))))
+           ;; ((0 1 2 3 4 5) 3) => (0 1 2 4 3 5)
+           ;; ((0 1 2 3 4 5) 5) => (5 1 2 3 4 0)
+           (when (and (listp ring) (not (null ring)))
+             (let* ((l (length ring))
+                    (n (mod n l)))
+               (when (>= l 2)
+                 (if (= n (- l 1))
+                     (append (last ring)
+                             (butlast (cdr ring))
+                             (list (car ring)))
+                     (append (subseq ring 0 n)
+                             (list (nth (mod (+ n 1) l) ring))
+                             (list (nth (mod (+ n 0) l) ring))
+                             (subseq ring (+ n 2)))))))))
+    (progn
+      (when opposite (setf n (- n 1)))
+      (symbol-macrolet
+          ((dyno (dyn-float-group-dyn-order group)))
+        (setf dyno (permute-at dyno n)))
+      (re-tile group))))
 
 (defcommand gnew-dyn-float
     (name) ((:rest "Group Name: "))
