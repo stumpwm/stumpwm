299c27df7747b3b02284d5dd8918a7aba206a755
Axel Svensson <mail@axelsvensson.com>
2021-06-13 11:54:08 +0200

    Add/improve integration test cases
    - Minimize and clarify 002
    - Add 003 and 004

--- tests/integration-tests/testcases/003.sh
+++ tests/integration-tests/testcases/003.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -ex
 
-# Test that frame indicator and frame outlines are updated correctly after resolution change
+# Test that the frame indicator is drawn correctly after `only`
 
 start-xvfb-with-max-resolution 2000 2000
 start-stumpwm
 set-resolution 640 480
 
-stumpwm-cmd hsplit
-set-resolution 1280 960
-screenshot 1
+stumpwm-cmd vsplit
 stumpwm-cmd only
-stumpwm-cmd hsplit
+screenshot 1
+stumpwm-cmd vsplit
+stumpwm-cmd move-focus down
+stumpwm-cmd only
 screenshot 2
 screenshots-match 1 2
