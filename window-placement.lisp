stumpwm/stumpwm

commit 5c077c7e94abf7079d187f8ccb9274144ea4cf83

Float windows in tiling groups using `define-frame-preference` (#792)
* modifies `define-frame-preference` to handle floats
* adds handling for a `:float` frame within `define-frame-preference`,
  `remember` and `group-add-window` in order
  to create a window as a floating window in a tiling group.
* The keys `:match-properties-and-function` and
  `:match-properties-or-function` are added in addition to *-not
  variants of all properties.
* A bug is fixed in `window-matches-properties-p` where type was not
  properly compared.
* Fixed possessive grammar in `define-frame-preference` docstring.
* fixed group reference, window reference
* Removed unnecessary &key arguments which are documented in docstring
* removed unnecessary d-bind arguments

window-placement.lisp
@@ -28,33 +28,60 @@
 (defun string-match (string pat)
   (ppcre:scan (get-or-create-rule-scanner pat) string))
 
-(defun window-matches-properties-p (window &key class instance type role title)
+(defun window-matches-properties-p (window
+                                    &key class class-not instance instance-not
+                                      type type-not role role-not title title-not)
   "Returns T if window matches all the given properties"
   (and
    (if class (string-match (window-class window) class) t)
+   (if class-not (not (string-match (window-class window) class-not)) t)
    (if instance (string-match (window-res window) instance) t)
-   (if type (string-match (window-type window) type) t)
+   (if instance-not (not (string-match (window-res window) instance-not)) t)
+   (if type (eq (window-type window) type) t)
+   (if type-not (not (eq (window-type window) type-not)) t)
    (if role (string-match (window-role window) role) t)
-   (if title (string-match (window-title window) title) t) t))
+   (if role-not (not (string-match (window-role window) role-not)) t)
+   (if title (string-match (window-title window) title) t)
+   (if title-not (not (string-match (window-title window) title-not)) t)
+   t))
 
 
 (defun window-matches-rule-p (w rule)
   "Returns T if window matches rule"
   (destructuring-bind (group-name frame raise lock
-                       &key from-group create restore class instance type role title) rule
+                       &key from-group create restore class class-not instance
+                         instance-not type type-not role role-not title title-not
+                         match-properties-and-function
+                         match-properties-or-function)
+      rule
     (declare (ignore frame raise create restore))
-    (let ((from-group (cond ((not from-group) (group-name (or (when (slot-boundp w 'group)
-                                                                (window-group w))
-                                                              (current-group))))
-                            ((stringp from-group) from-group)
-                            (t (group-name (eval from-group))))))
-      (if (or lock
-              (equal group-name from-group))
-          (window-matches-properties-p w :class class
-                                         :instance instance
-                                         :type type
-                                         :role role
-                                         :title title)))))
+    (let* ((from-group (cond ((not from-group)
+                              (group-name (or (when (slot-boundp w 'group)
+                                                (window-group w))
+                                              (current-group))))
+                             ((stringp from-group) from-group)
+                             (t (group-name (eval from-group)))))
+           (properties-matched
+             (if (or lock (equal group-name from-group))
+                 (window-matches-properties-p w :class class
+                                                :class-not class-not
+                                                :instance instance
+                                                :instance-not instance-not
+                                                :type type
+                                                :type-not type-not
+                                                :role role
+                                                :role-not role-not
+                                                :title title
+                                                :title-not title-not))))
+      (cond ((and match-properties-and-function match-properties-or-function)
+             (or (and properties-matched
+                      (funcall match-properties-and-function w))
+                 (funcall match-properties-or-function w)))
+            (match-properties-or-function
+             (or properties-matched (funcall match-properties-or-function w)))
+            (match-properties-and-function
+             (and properties-matched (funcall match-properties-or-function w)))
+            (t properties-matched)))))
 
 (defun rule-matching-window (window)
   (dolist (rule *window-placement-rules*)
@@ -66,8 +93,8 @@
   (let ((match (rule-matching-window window)))
     (if match
         (destructuring-bind (group-name frame raise lock
-                             &key from-group create restore class instance type role title) match
-          (declare (ignore from-group lock class instance type role title))
+                             &key create restore) match
+          (declare (ignore from-group lock))
           (let ((group (find-group screen group-name)))
             (cond (group
                    (when (and restore (stringp restore))
@@ -77,7 +104,11 @@
                                           (read-dump-from-file restore-file))
                            (message "^B^1*Can't restore group \"^b~a^B\" with \"^b~a^B\"."
                                     group-name restore-file))))
-                   (values group (frame-by-number group frame) raise))
+                   (values group
+                           (if (eq frame :float)
+                               frame
+                               (frame-by-number group frame))
+                           raise))
                   (create
                    (let ((new-group (add-group (current-screen) group-name))
                          (restore-file (if (stringp create)
@@ -90,9 +121,19 @@
                          (when (stringp create)
                            (message "^B^1*Can't restore group \"^b~a^B\" with \"^b~a^B\"."
                                     group-name restore-file)))
-                     (values new-group (frame-by-number new-group frame) raise)))
-                    (t (message "^B^1*Error placing window, group \"^b~a^B\" does not exist." group-name)
-                       (values)))))
+                     (values new-group
+                             (if (eq frame :float)
+                                 frame
+                                 (frame-by-number group frame))
+                             raise)))
+                  ((not group-name)
+                   (values (current-group)
+                           (if (eq frame :float)
+                               frame
+                               (frame-by-number (current-group) frame))
+                           raise))
+                  (t (message "^B^1*Error placing window, group \"^b~a^B\" does not exist." group-name)
+                     (values)))))
         (values))))
 
 (defun sync-single-window-placement (screen window &optional show)

primitives.lisp
@@ -1121,10 +1121,16 @@ add rules")
   "Create a rule that matches windows and automatically places them in
 a specified group and frame. Each frame rule is a lambda list:
 @example
-\(frame-number raise lock &key from-group create restore dump-name class instance type role title)
+\(frame-number raise lock &key from-group create restore dump-name class class-not
+instance instance-not type type-not role role-not title title-not 
+match-properties-and-function match-properties-or-function)
 @end example
 
 @table @var
+@item target-group
+When nil, rule applies in the current group. When non nil, @var{lock} determines 
+applicability of rule
+
 @item frame-number
 The frame number to send matching windows to
 
@@ -1154,27 +1160,47 @@ When non-NIL the group is restored even if it already exists. This arg should
 be set to the dump filename to use for forced restore. Defaults to NIL
 
 @item class
-The window's class must match @var{class}.
+The windows class must match @var{class}.
+
+@item class-not
+The windows class must not match @var{class-not}
 
 @item instance
-The window's instance/resource name must match @var{instance}.
+The windows instance/resource name must match @var{instance}.
+
+@item instance-not
+The windows instance/resource name must not match @var{instance-not}.
 
 @item type
-The window's type must match @var{type}.
+The windows type must match @var{type}.
+
+@item type-not
+The windows type must not match @var{type-not}.
 
 @item role
-The window's role must match @var{role}.
+The windows role must match @var{role}.
+
+@item role-not
+The windows role must not match @var{role-not}.
 
 @item title
-The window's title must match @var{title}.
+The windows title must match @var{title}.
+
+@item title-not
+The windows title must not match @var{title-not}.
+
+@item match-properties-and-function
+A function that, if provided, must return true alongside the provided properties 
+in order for the rule to match. This function takes one argument, the window. 
+
+@item match-properties-or-function
+A function that, if provided and returning true, will cause the rule to match
+regardless of whether the window properties match. Takes one argument, the window.
 @end table"
   (let ((x (gensym "X")))
     `(dolist (,x ',frame-rules)
        ;; verify the correct structure
-       (destructuring-bind (frame-number raise lock
-                                         &rest keys
-                                         &key from-group create restore class instance type role title) ,x
-         (declare (ignore from-group create restore class instance type role title))
+       (destructuring-bind (frame-number raise lock &rest keys) ,x
          (push (list* ,target-group frame-number raise lock keys)
                *window-placement-rules*)))))
 

tile-group.lisp
@@ -64,10 +64,14 @@
           (tile-w (frame-raise-window group (window-frame tile-w) tile-w))
           (t (no-focus group nil)))))
 
-(defmethod group-add-window ((group tile-group) window &key frame raise &allow-other-keys)
+(defmethod group-add-window ((group tile-group) (window tile-window) &key frame raise &allow-other-keys)
   ;; This is important to get the frame slot
-  (if (typep window 'float-window)
-      (call-next-method)
+  (if (eq frame :float)
+      (progn
+        (change-class window 'float-window)
+        (float-window-align window)
+        (when raise
+          (group-focus-window group window)))
       (progn
         (change-class window 'tile-window)
         ;; Try to put the window in the appropriate frame for the group.

tile-window.lisp
@@ -396,9 +396,11 @@ frame. Possible values are:
   "Guess at a placement rule for WINDOW and add it to the current set."
   (let* ((group (window-group window))
          (group-name (group-name group))
-         (frame-number (frame-number (window-frame window)))
+         (frame-number-or-float (if (typep window 'float-window)
+                                    :float
+                                    (frame-number (window-frame window))))
          (role (window-role window)))
-    (push (list group-name frame-number t lock
+    (push (list group-name frame-number-or-float t lock
                 :class (window-class window)
                 :instance (window-res window)
                 :title (and title (window-name window))

commit d8c9fbc01497a615c334e44defb311b960a7d091

Typo fix: group -> new-group
Fixed typo introduced in 5c077c7e94abf7079d187f8ccb9274144ea4cf83

window-placement.lisp
@@ -125,7 +125,7 @@
                      (values new-group
                              (if (eq frame :float)
                                  frame
-                                 (frame-by-number group frame))
+                                 (frame-by-number new-group frame))
                              raise)))
                   ((not group-name)
                    (values (current-group)