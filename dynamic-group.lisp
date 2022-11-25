695fb57e1c1348f86be44b56b86b16ea63c616fb
szos <szos@posteo.net>
2022-11-25 19:27:17 +0100

    patch dynamic-group-resize-head to not map over window
    previously this attempted to map a function over a window, this patches it to
    call the function on the window.

--- dynamic-group.lisp
+++ dynamic-group.lisp
@@ -722,7 +722,7 @@ floating windows onto the stack."
                    (push window previous-floats)
                    (dynamic-mixins:replace-class window 'dynamic-window))
                  (dynamic-group-place-window group head window)
-              finally (map nil #'sync-minor-modes window))
+              finally (sync-minor-modes window))
         (focus-frame group (window-frame master-window))))))
 
 ;;; Handle overflow of both heads and groups
